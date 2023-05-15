8752142c6884d711da7bed04f51635858d988948
unknown <36slices@gmail.com>
2023-05-16 11:49:14 +1200

    leaving farm, making progress on different examples

--- ADP-3450/C#/ADP-3450-WaveFormGen-PG/ADP-3450-WaveFormGen-PG/Program.cs
+++ ADP-3450/C#/ADP-3450-WaveFormGen-PG/ADP-3450-WaveFormGen-PG/Program.cs
@@ -1,42 +1,72 @@
-﻿/*
+﻿/* Author: Matthew Burton 16/05/23
  * ------------------------------------------------------------------------------
- * Code for creating a sweeping sine wave that fluctuates in speed
+ * 
  * ------------------------------------------------------------------------------
 */
+int nSamples = 100000;
 int hdwf;
-int idxChannel = 0;
-double hzStart = 1e3;
-double hzStop = 1e5;
-double secSweep = 1;
+byte sts;
+double hzAcq = 50000.0;
+int cSamples = 0;
+int cAvailable, cLost, cCorrupted;
+double[] rgdSamples;
+bool fLost = false, fCorrupted = false;
 
-// Open Device
-// Opens a device identified by the enumeration index and retrieves a handle. To automatically
-//enumerate all connected devices and open the first discovered device, use index - 1.
+Console.WriteLine("Open automatically the first available device");
 dwf.FDwfDeviceOpen(-1, out hdwf);
 
-// enable first channel
-dwf.FDwfAnalogOutNodeEnableSet(hdwf, idxChannel, dwf.AnalogOutNodeCarrier, 1);
-// set sine carrier
-dwf.FDwfAnalogOutNodeFunctionSet(hdwf, idxChannel, dwf.AnalogOutNodeCarrier, dwf.funcSine);
-// 1V amplitude, 2V pk2pk
-dwf.FDwfAnalogOutNodeAmplitudeSet(hdwf, idxChannel, dwf.AnalogOutNodeCarrier, 1.0);
-// 10kHz carrier frequency
-dwf.FDwfAnalogOutNodeFrequencySet(hdwf, idxChannel, dwf.AnalogOutNodeCarrier, (hzStart + hzStop) / 2);
-// enable frequency modulation
-dwf.FDwfAnalogOutNodeEnableSet(hdwf, idxChannel, dwf.AnalogOutNodeFM, 1);
-// linear sweep with ramp up symmetry 100%
-dwf.FDwfAnalogOutNodeFunctionSet(hdwf, idxChannel, dwf.AnalogOutNodeFM, dwf.funcRampUp);
-dwf.FDwfAnalogOutNodeSymmetrySet(hdwf, idxChannel, dwf.AnalogOutNodeFM, 100);
-// modulation index
-dwf.FDwfAnalogOutNodeAmplitudeSet(hdwf, idxChannel, dwf.AnalogOutNodeFM, 100.0 * (hzStop - hzStart) / (hzStart + hzStop));
-// FM frequency = 1/sweep time
-dwf.FDwfAnalogOutNodeFrequencySet(hdwf, idxChannel, dwf.AnalogOutNodeFM, 1.0 / secSweep);
+dwf.FDwfAnalogInChannelEnableSet(hdwf, 0, 1);
+dwf.FDwfAnalogInChannelRangeSet(hdwf, 0, 5);
 
-// by default the offset is 0V
-// start signal generation
-dwf.FDwfAnalogOutConfigure(hdwf, idxChannel, 1);
-// it will run until stopped or device closed
+// recording rate for more samples than the device buffer is limited by device communication
+dwf.FDwfAnalogInAcquisitionModeSet(hdwf, dwf.acqmodeRecord);
+dwf.FDwfAnalogInFrequencySet(hdwf, hzAcq);
+dwf.FDwfAnalogInRecordLengthSet(hdwf, 1.0 * nSamples / hzAcq);
+
+// start
+dwf.FDwfAnalogInConfigure(hdwf, 0, 1);
+
+Console.WriteLine("Recording...\n");
+
+while (cSamples < nSamples)
+{
+    dwf.FDwfAnalogInStatus(hdwf, 1, out sts);
+
+    if (sts == dwf.stsError)
+    {
+        Console.WriteLine("error");
+    }
+    if (cSamples == 0 && (sts == dwf.stsCfg || sts == dwf.stsPrefill || sts == dwf.stsArm))
+    {
+        // Acquisition not yet started.
+        continue;
+    }
+
+    dwf.FDwfAnalogInStatusRecord(hdwf, out cAvailable, out cLost, out cCorrupted);
+
+    cSamples += cLost;
+
+    if (cLost > 0) fLost = true;
+    if (cCorrupted > 0) fCorrupted = true;
+
+    if (cAvailable == 0) continue;
+
+    // get samples
+    rgdSamples = new double[cSamples];
+    dwf.FDwfAnalogInStatusData(hdwf, 0, rgdSamples, cAvailable);
+    cSamples += cAvailable;
+}
+
 Console.WriteLine("done\n");
 
-// on close device is stopped and configuration lost
-dwf.FDwfDeviceCloseAll();
+if (fLost)
+{
+    Console.WriteLine("Samples were lost! Reduce frequency");
+}
+else if (fCorrupted)
+{
+    Console.WriteLine("Samples could be corrupted! Reduce frequency");
+}
+
+// close the device
+dwf.FDwfDeviceClose(hdwf);
