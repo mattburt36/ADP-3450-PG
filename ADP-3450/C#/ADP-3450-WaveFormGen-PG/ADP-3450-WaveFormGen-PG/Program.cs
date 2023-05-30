89f9a98366b16704eec36e054a1a127ba63f9743
Matthew Burton <36slices@gmail.com>
2023-05-31 10:36:33 +1200

    Made application code, need to test

--- ADP-3450/C#/ADP-3450-WaveFormGen-PG/ADP-3450-WaveFormGen-PG/Program.cs
+++ ADP-3450/C#/ADP-3450-WaveFormGen-PG/ADP-3450-WaveFormGen-PG/Program.cs
@@ -3,12 +3,148 @@
  * 
  * ------------------------------------------------------------------------------
 */
-int hdwf;
+using System;
+using System.Runtime.InteropServices;
 
-Console.WriteLine("Open automatically the first available device");
-dwf.FDwfDeviceOpen(-1, out hdwf);
+public class ADP3450Application
+{
+    // DWF constants
+    public const int Ok = 0;
+    public const int DeviceNotConfigured = -1;
+    // Add more constants as needed
 
+    // Add more function declarations as needed
 
+    private int deviceHandle; // Store the device handle
 
-// close the device
-dwf.FDwfDeviceClose(hdwf);
+    public bool OpenDevice()
+    {
+        // Open the ADP3450 device
+        int result = dwf.FDwfDeviceOpen(0, out deviceHandle);
+        if (result == Ok)
+        {
+            // Device opened successfully
+            return true;
+        }
+        else
+        {
+            // Error occurred while opening the device
+            return false;
+        }
+    }
+
+    public void CloseDevice()
+    {
+        // Close the ADP3450 device
+        if (deviceHandle != DeviceNotConfigured)
+        {
+            dwf.FDwfDeviceClose(deviceHandle);
+            deviceHandle = DeviceNotConfigured;
+        }
+    }
+
+    public void GenerateSignal()
+    {
+        // Configure and generate signals using the provided DWF functions
+        // Example usage:
+        // FDwfAnalogOutEnableSet(deviceHandle, channel, 1); // Enable analog output channel
+        // FDwfAnalogOutFunctionSet(deviceHandle, channel, func); // Set function for the channel
+        // FDwfAnalogOutFrequencySet(deviceHandle, channel, frequency); // Set frequency for the channel
+        // FDwfAnalogOutAmplitudeSet(deviceHandle, channel, amplitude); // Set amplitude for the channel
+        // ... continue configuring other settings and generating the signal
+        // Example: Generate a sine wave signal on channel 1
+
+        int channel = 0; // Assuming channel 1 is used
+        double frequency = 1000; // Frequency in Hz
+        double amplitude = 1.0; // Amplitude in Volts
+
+        // Enable analog output channel
+        dwf.FDwfAnalogOutEnableSet(deviceHandle, channel, 1);
+
+        // Set function for the channel (e.g., Sine, Square, etc.)
+        dwf.FDwfAnalogOutFunctionSet(deviceHandle, channel, dwf.funcSine);
+
+        // Set frequency for the channel
+        dwf.FDwfAnalogOutFrequencySet(deviceHandle, channel, frequency);
+
+        // Set amplitude for the channel
+        dwf.FDwfAnalogOutAmplitudeSet(deviceHandle, channel, amplitude);
+
+        // Start generating the signal
+        dwf.FDwfAnalogOutConfigure(deviceHandle, channel, 1);
+    }
+
+    public void ListenToSignal()
+    {
+        // Configure and listen to signals using the provided DWF functions
+        // Example usage:
+        // FDwfAnalogInConfigure(deviceHandle, 1, 1); // Configure analog input channel
+        // ... continue configuring other settings and listening to the signal
+        // Example: Listen to channel 1
+        int channel = 0; // Assuming channel 1 is used
+        int bufferSize = 8192; // Buffer size for the channel
+        double frequency = 1000; // Frequency in Hz
+
+        // Configure analog input channel
+        dwf.FDwfAnalogInConfigure(deviceHandle, channel, 1);
+
+        // Set buffer size for the channel
+        dwf.FDwfAnalogInBufferSizeSet(deviceHandle, bufferSize);
+
+        // Set frequency for the channel
+        dwf.FDwfAnalogInFrequencySet(deviceHandle, frequency);
+
+        // Enable analog input channel
+        dwf.FDwfAnalogInChannelEnableSet(deviceHandle, channel, 1);
+
+        // Start listening to the signal
+        dwf.FDwfAnalogInConfigure(deviceHandle, channel, 1);
+
+        // Create a buffer to store the acquired data
+        double[] buffer = new double[bufferSize];
+
+        // Read the acquired data from the device
+        dwf.FDwfAnalogInStatusData(deviceHandle, channel, buffer, bufferSize);
+
+        // Process the acquired data
+        // Example: Print the acquired data
+        for (int i = 0; i < bufferSize; i++)
+        {
+            Console.WriteLine(buffer[i]);
+        }
+    }
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        ADP3450Application app = new ADP3450Application();
+
+        // Open the ADP3450 device
+        if (app.OpenDevice())
+        {
+            try
+            {
+                // Generate signals
+                app.GenerateSignal();
+
+                // Listen to signals
+                app.ListenToSignal();
+
+                // Add any other logic or user interaction here
+
+                // Close the ADP3450 device
+                app.CloseDevice();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("An error occurred: " + ex.Message);
+            }
+        }
+        else
+        {
+            Console.WriteLine("Failed to open the ADP3450 device.");
+        }
+    }
+}
